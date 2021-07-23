86a735593073772718022511ddda59577c0a09ed
jasmintey <65060477+jasmintey@users.noreply.github.com>
2021-07-23 09:19:54 +0800

    lab6

--- demoVcs.py
+++ demoVcs.py
@@ -8,6 +8,13 @@ class Calculator:
         minus = num1 - num2
         print(minus)
         return minus
+
+    def multiply(num1, num2):
+        multiply = num1 * num2
+        print(multiply)
+        return multiply
+
 Calculator.add(50,40)
 Calculator.minus(50,40)
+Calculator.multiply(50,40)
 
