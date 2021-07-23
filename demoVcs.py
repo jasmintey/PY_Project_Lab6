b3c212477a68075a73c658bb1181b486eb4913b2
jasmintey <65060477+jasmintey@users.noreply.github.com>
2021-07-23 09:16:39 +0800

    Lab6

---
+++ demoVcs.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def add(num1, num2):
+        add = num1 + num2
+        print(add)
+        return add
+
+    def minus(num1, num2):
+        minus = num1 - num2
+        print(minus)
+        return minus
+Calculator.add(50,40)
+Calculator.minus(50,40)
+
